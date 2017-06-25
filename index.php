0c3bf64de7bdd7ea9751d30250cf0676617e3da8
Ridvan Baluyos <ridvan@baluyos.net>
2017-06-25 22:23:54 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+	<title>Gundam Server</title>
+	<meta name="author" content="Ridvan Baluyos <www.ridvanbaluyos.com>">
+	<meta name="description" content="This is the Gundam Server.">
+	<meta name="keywords" content="Gundam, Gundam Server, Philippines, Software Company" />
+   	<meta http-equiv="X-UA-Compatible" content="IE=9" />
+   	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+   	
+    <!-- Facebook Meta -->
+    <meta property="og:title" content="Gundam Server" />
+    <meta property="og:site_name" content="Gundam Server" />
+    <meta property="og:locale" content="en_PH" />
+    <meta property="og:url" content="https://gundamserver.com" />
+    <meta property="og:description" content="Gundam Server" />
+    <meta property="og:image" content="https://gundamserver.com/gundam-head.jpg" />
+    <meta property="og:type" content="website" />
+ 
+    <!-- Google Plus Meta -->
+    <link rel="author" href="//plus.google.com/+RidvanBaluyos" />
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+	<link href='//fonts.googleapis.com/css?family=Orbitron' rel='stylesheet' type='text/css'>
+  	<link rel="stylesheet" href="app.css">
+
+	<!--[if lt IE 9]>
+		<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.js"></script>
+    <body>
+        <img src="gundam-head.jpg" class="ri" alt="Gundam Exia Head" title="Gundam Exia Head" />
+        <span align="center" class="title">GundamServer.com </span>      
+    </body>
+ </html> 
